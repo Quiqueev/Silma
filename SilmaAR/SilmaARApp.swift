dfd26bc4b4b70c75bc811b1c63c326642b0fdedd
ricardomongza <ricardomongza@gmail.com>
2023-04-05 10:34:39 -0500

    Add all contents of Xcode app again (to apply previously forgotten .gitignore)

---
+++ SilmaAR/SilmaARApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilmaARApp.swift
+//  SilmaAR
+//
+//  Created by Ricardo Montemayor on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SilmaARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
